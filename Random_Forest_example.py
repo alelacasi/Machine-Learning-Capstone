3ad3aab8e7397cc2f823349a03b3aa54824ccc6e
ale <ale-laca@hotmail.com>
2020-12-29 22:50:39 -0300

    Random Forests Example
    RF example with Calssification and Regression. Not quite sure about the RMSE

--- Random_Forest_example.py
+++ Random_Forest_example.py
@@ -1,9 +1,77 @@
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.datasets import load_breast_cancer
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler 
+from sklearn import metrics
+from sklearn.metrics import r2_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import pandas as pd
 import numpy as np
 from sklearn import tree
+
+#!Random Forest for Regession
+
+# Reading the information from the csv file into the dataset
+datasets = pd.read_csv('petrol_consumption.csv')
+datasets.head()
+
+# Prepearing the Data for training
+#* Data must be divided into attributes and labels
+X = datasets.iloc[:,0:4].values
+Y = datasets.iloc[:,4].values
+
+#Dividing the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(X,Y, test_size=0.2, random_state = 0)
+
+# Feature scaling
+#* We need to do this because our values in the dataset are in different scales, some in tens others in thousands
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.fit_transform(x_test)
+
+#Training Phase using Random Forests
+#* n_estimators is the number of trees in the random forest
+regressor = RandomForestRegressor(n_estimators=20,random_state = 0)
+regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
+
+#Evaluating the algorithm
+print("--------------------------------Evaluating Regression--------------------------------")
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+print('Root Mean Squared Error:', r2_score(y_test, y_pred))
+
+# #!Random Forest for Classification
+
+# Reading the information from the csv file into the dataset
+datasets = pd.read_csv('bill_authentication.csv')
+datasets.head()
+
+# Prepearing the Data for training
+#* Data must be divided into attributes and labels
+X = datasets.iloc[:,0:4].values
+Y = datasets.iloc[:,4].values
+
+#Dividing the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(X,Y, test_size=0.2, random_state = 0)
+
+# Feature scaling
+#* We need to do this because our values in the dataset are in different scales, some in tens others in thousands
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.fit_transform(x_test)
+
+#Training Phase using Random Forests
+#* n_estimators is the number of trees in the random forest
+classifier = RandomForestClassifier(n_estimators=20,random_state = 0)
+classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
+
+#Evaluating the algorithm
+print("--------------------------------Evaluating Classification--------------------------------")
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+print('Accuracy: ',accuracy_score(y_test, y_pred))
